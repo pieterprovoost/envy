2e4a27c0935567295c7e219316bafda5d1c16a8c
MMIODE12 <pieterprovoost@gmail.com>
2015-06-26 14:03:20 +0200

    stacked area chart

--- envy.js
+++ envy.js
@@ -1,4 +1,72 @@
 angular.module('envy', [])
+	.directive('envyStackedArea', function($timeout, envydata) {
+		return {
+			restrict: "E",
+			replace: true,
+			scope: {
+				data: '=?',
+				options: '=?'
+			},
+			transclude: false,
+			template: '<div class="envy envystackedarea"><svg></svg></div>',
+			controller: ["$scope", function ($scope) {
+			}],
+			link: function(scope, element, attrs, controller) {
+
+				var default_options = {
+					tooltips: true,
+					xFormat: d3.format('.02f'),
+					yFormat: d3.format('.02f'),
+					style: 'stack',
+					showControls: false,
+					showLegend: true,
+					color: nv.utils.defaultColor(),
+					duration: 0,
+					margin: {top: 30, right: 25, bottom: 50, left: 60},
+					xAxisLabel: "",
+					yAxisLabel: ""
+				};
+
+				scope.$watch('data', function() {
+
+					scope.options = angular.extend(default_options, scope.options);
+
+					if(!angular.isDefined(scope.data) || scope.data.length == 0) {
+						d3.selectAll(element[0].firstChild.childNodes).remove();
+						element[0].classList.add('empty');
+						envydata.removeChart(attrs.id);
+					} else {
+						element[0].classList.remove('empty');
+						nv.addGraph(function() {
+							var chart = nv.models.stackedAreaChart()
+								.tooltips(scope.options.tooltips)
+								.style(scope.options.style)
+								.showControls(scope.options.showControls)
+								.showLegend(scope.options.showLegend)
+								.color(scope.options.color)
+								.duration(scope.options.duration)
+								.margin(scope.options.margin);
+							chart.xAxis.tickFormat(scope.options.xFormat).axisLabel(scope.options.xAxisLabel);
+							chart.yAxis.tickFormat(scope.options.yFormat).axisLabel(scope.options.yAxisLabel);
+							d3.select(element[0].firstChild)
+								.datum(scope.data)
+								.call(chart);
+							nv.utils.windowResize(chart.update);
+
+							scope.$on('$destroy', function () {
+								envydata.reset();
+							});
+
+							envydata.setChart(attrs.id, chart);
+
+							return chart;
+						});
+					}
+
+				});
+			}
+		}
+	})
 	.directive('envyMultiBar', function($timeout, envydata) {
 		return {
 			restrict: "E",
